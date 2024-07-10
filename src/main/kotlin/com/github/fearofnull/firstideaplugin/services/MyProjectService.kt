d808184028563aab2ab86cc5380d00917a3f9990
GitHub Action <action@github.com>
2024-07-10 02:29:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/fearofnull/firstideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fearofnull/firstideaplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.fearofnull.firstideaplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fearofnull.firstideaplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
